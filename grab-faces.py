dfd50cdfbd637e2d2bc77e21afde002aa684d3d2
luke-slytalker <73183410+luke-slytalker@users.noreply.github.com>
2022-08-14 19:57:42 -0700

    Update grab-faces.py

--- grab-faces.py
+++ grab-faces.py
@@ -15,21 +15,19 @@ if not os.path.exists("./faces"):
     
 savepath = "./faces"
 
-nof = 0
+nof = 0     # varialbe to hold the number of faces found (and give an iterator for naming each face)
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     # checking if it is a file
     if os.path.isfile(f):
         print(f)
-        if f.endswith(".png") or f.endswith(".jpg") or f.endswith(".jpeg"):
+        if f.endswith(".png") or f.endswith(".jpg") or f.endswith(".jpeg"):     # not-so-great way to check to make sure it's an image we're grabbing
 
-            # Load the jpg file into a numpy array
+            # Load the image file into a numpy array
             image = face_recognition.load_image_file(f)
 
             # Find all the faces in the image using the default HOG-based model.
-            # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-            # See also: find_faces_in_picture_cnn.py
             face_locations = face_recognition.face_locations(image)
 
             print("I found {} face(s) in this photograph.".format(len(face_locations)))
@@ -44,5 +42,7 @@ for filename in os.listdir(directory):
                 face_image = image[top:bottom, left:right]
                 pil_image = Image.fromarray(face_image)
                 #pil_image.show()
-                pil_image.save(savepath + "/face_" + str(nof) + ".png", format='png')
+                pil_image.save(savepath + "/face_" + str(nof) + ".png", format='png')       # save the image
                 print("FACE " + str(nof) + " saved.")
+
+                
